4a2c74c1f1f1fd5480b04b909428f275723dbe43
TerenceWin <105475764+TerenceWin@users.noreply.github.com>
2026-04-17 12:49:46 +0900

    Fix left Panel enlarging bug

---
+++ take-home-accessment/src/components/Airbnb.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import './Airbnb.css'
+
+function Airbnb(){
+    
+    return(
+        <div className="AirbnbContainer">
+            
+        </div>
+    )
+}
+
+export default Airbnb
